b221cb2a582c92d4ba8ec8cc36bcb8b1c3550867
arasvan <arasvan@users.noreply.github.com>
2021-01-29 13:28:02 -0800

    nodejs connected to mongodb atlas

--- server/server.js
+++ server/server.js
@@ -2,80 +2,29 @@ const express = require("express");
 const app = express();
 
 const PORT = 8080;
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server started. Port ${PORT}`));
 
-let lessons = [
-    {
-        "subject": "Maths",
-        "location": "Brent Cross",
-        "price": 100,
-        "space": 5,
-        "icon": "graphics/maths_icon.png"
-    },
-    {
-        "subject": "English",
-        "location": "Wembley",
-        "price": 80,
-        "space": 5,
-        "icon": "graphics/english_icon.png"
-    },
-    {
-        "subject": "Maths",
-        "location": "Wembley",
-        "price": 110,
-        "space": 5,
-        "icon": "graphics/maths_icon.png"
-    },
-    {
-        "subject": "English",
-        "location": "Brent Cross",
-        "price": 90,
-        "space": 5,
-        "icon": "graphics/english_icon.png"
-    },
-    {
-        "subject": "Maths",
-        "location": "Chelsea",
-        "price": 95,
-        "space": 5,
-        "icon": "graphics/maths_icon.png"
-    },
-    {
-        "subject": "Physics",
-        "location": "Edgeware",
-        "price": 110,
-        "space": 5,
-        "icon": "graphics/physics_icon.png"
-    },
-    {
-        "subject": "Chemistry",
-        "location": "Brent Cross",
-        "price": 65,
-        "space": 5,
-        "icon": "graphics/chemistry_icon.png"
-    },
-    {
-        "subject": "Biology",
-        "location": "Finchley",
-        "price": 115,
-        "space": 5,
-        "icon": "graphics/biology_icon.png"
-    },
-    {
-        "subject": "Music",
-        "location": "Greenford",
-        "price": 75,
-        "space": 5,
-        "icon": "graphics/music_icon.png"
-    },
-    {
-        "subject": "Physics",
-        "location": "Hampton",
-        "price": 80,
-        "space": 5,
-        "icon": "graphics/physics_icon.png"
-    }
-];
+const mongoClient = require("mongodb").MongoClient;
+
+let db;
+mongoClient.connect('mongodb+srv://arasvan:219031@mycluster.ybtia.mongodb.net/cw2?retryWrites=true&w=majority/', (err, client) => {
+    db = client.db('cw2');
+    })
+
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName);
+    return next();
+})
+
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.send(results);
+    })
+})
+
+let lessons = [];
 
 app.get("/lessons", function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
