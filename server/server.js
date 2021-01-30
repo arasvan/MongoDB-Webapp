02887f9bcbadb6fa8d6015957d43935b29911fdc
arasvan <arasvan@users.noreply.github.com>
2021-01-29 17:01:29 -0800

    subtract lesson spaces from database

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.param('collectionName', (req, res, next, collectionName) => {
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     next();
 })
 
@@ -36,15 +37,24 @@ app.get('/collection/:collectionName', (req, res, next) => {
 
 app.post('/collection/:collectionName', (req, res, next) => {
     req.collection.insertOne(req.body, (e, results) => {
-        if (e) return next(e) 
-        res.send(results.ops)
+        if (e) return next(e);
+        res.send(results.ops);
     })
 })
 
+const ObjectID = require("mongodb").ObjectID;
 app.put('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.update(
         { _id: new ObjectID(req.params.id) },
-        { $set: req.body },
+        {
+            $set: { 
+                subject: req.body.subject,
+                location: req.body.location,
+                price: req.body.price,
+                space: req.body.space,
+                icon: req.body.icon
+            }
+        },
         { safe: true, multi: false },
         (e, result) => {
             if (e) return next(e)
