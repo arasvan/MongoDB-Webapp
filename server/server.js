c30841880298d7d19d2b858ca5145ef465942337
arasvan <arasvan@users.noreply.github.com>
2021-01-29 16:17:19 -0800

    add orders to mongodb

--- server/server.js
+++ server/server.js
@@ -1,37 +1,42 @@
 const express = require("express");
 const app = express();
 
-const PORT = 8080;
+var bodyParser = require('body-parser');
+
+const PORT = 3000;
 app.listen(PORT, console.log(`Server started. Port ${PORT}`));
 
 const mongoClient = require("mongodb").MongoClient;
 
 let db;
-mongoClient.connect('mongodb+srv://arasvan:219031@mycluster.ybtia.mongodb.net/cw2?retryWrites=true&w=majority/', (err, client) => {
+mongoClient.connect('mongodb+srv://arasvan:219031@mycluster.ybtia.mongodb.net/', (err, client) => {
     db = client.db('cw2');
-    })
+})
 
 app.param('collectionName', (req, res, next, collectionName) => {
     req.collection = db.collection(collectionName);
     return next();
 })
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/collection/:collectionName', (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
         if (e) return next(e);
-        res.setHeader('Access-Control-Allow-Origin', '*');
         res.send(results);
     })
 })
 
-let lessons = [];
-
-app.get("/lessons", function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(JSON.stringify(lessons));
-});
-
-app.get("/user", function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(JSON.stringify(user));
-});
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insertOne(req.body, (e, results) => {
+        if (e) return next(e) 
+        res.send(results.ops)
+    })
+})
